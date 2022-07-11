1ee89afb0042c28ce82f708ece98870cb474c2a0
DARTwayder <whiteroom98@gmail.com>
2022-07-11 23:39:31 +0300

    new addons

--- lib/data/repositories/list_with_ids_reactive_repository.dart
+++ lib/data/repositories/list_with_ids_reactive_repository.dart
@@ -1,50 +1,12 @@
-import 'package:rxdart/rxdart.dart';
+import 'package:flutter/foundation.dart';
+import 'package:memogenerator/data/repositories/list_reactive_repository.dart';
 import 'package:collection/collection.dart';
 
-abstract class ListWithIdsReactiveRepository<T> {
-  final updater = PublishSubject<Null>();
+abstract class ListWithIdsReactiveRepository<T>
+    extends ListReactiveRepository<T> {
 
-  Future<List<String>> getRawData();
-
-  Future<bool> saveRawData(final List<String> items);
-
-  T convertFromString(final String rawItem);
-
-  String convertToString(final T item);
-
+  @protected
   dynamic getId(final T item);
-
-  // еще 2 доп Метода
-  Future<List<T>> getItems() async {
-    final rawItems = await getRawData();
-    return rawItems.map((rawItem) => convertFromString(rawItem)).toList();
-  }
-
-  Future<bool> setItems(final List<T> items) async {
-    final rawMemes = items.map((item) => convertToString(item)).toList();
-    return _setRawItems(rawMemes);
-  }
-
-// Метод будет выдавать список со всем избранным,которые будут обображатся на мейн странице при входе на экран
-// Метод Observe
-  Stream<List<T>> observeItems() async* {
-    yield await getItems();
-    await for (final _ in updater) {
-      yield await getItems();
-    }
-  }
-
-  Future<bool> addItem(final T item) async {
-    final items = await getItems();
-    items.add(item);
-    return setItems(items);
-  }
-
-  Future<bool> removeItem(final T item) async {
-    final items = await getItems();
-    items.remove(item);
-    return setItems(items);
-  }
 
   //Метод добавления в избраное
   Future<bool> addItemOrReplaceById(final T newItem) async {
@@ -67,15 +29,9 @@ abstract class ListWithIdsReactiveRepository<T> {
     return setItems(items);
   }
 
-
 //Метод оффлайн просмотра избранного
   Future<T?> getItemById(final dynamic id) async {
     final items = await getItems();
     return items.firstWhereOrNull((item) => getId(item) == id);
-  }
-
-  Future<bool> _setRawItems(final List<String> rawItems) {
-    updater.add(null);
-    return saveRawData(rawItems);
   }
 }
